18420aa3843ed26b8c6652aaad00ec129493b6fe
Daniel <daniellsoouza380@gmail.com>
2025-03-05 17:13:54 -0300

    sending initial files

---
+++ ecommerce/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='index' options={{headerShown: false}}/>
+      <Stack.Screen name='next' options={{headerShown: false}}/>
+    </Stack>
+    
+  )
+}
+
+export default RootLayout
